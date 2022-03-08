7036d4d24be392bc0eeb4cb70223ea0466687bcf
wangshouchun <wangshouchun@xiaomi.com>
2022-03-08 11:30:40 +0800

    Avoid Installer APP ANR when starting install in main thread
    Fixes:223240913
    Test:manual
    
    Signed-off-by: wangshouchun <wangshouchun@xiaomi.com>
    Change-Id: I71d06ed344e0baa6ea27abc1e6e886fc00ed8b3a

--- services/core/java/com/android/server/pm/PackageInstallerSession.java
+++ services/core/java/com/android/server/pm/PackageInstallerSession.java
@@ -3467,9 +3467,7 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
      * @return the uid of the owner this session
      */
     public int getInstallerUid() {
-        synchronized (mLock) {
-            return mInstallerUid;
-        }
+        return mInstallerUid;
     }
 
     /**
